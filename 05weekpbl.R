a62248d7dc1d374efa02937c68f3d82be84a211b
심지원 <simjiwon@simjiwon-ui-MacBookPro.local>
2023-10-04 15:55:53 +0900

    hi

---
+++ 05weekpbl.R
@@ -0,0 +1,12 @@
+install.packages("RCurl")
+install.packages("XML")
+install.packages("wordcloud")
+install.packages("RmecabKo")
+
+library(RCurl)
+library(XML)
+library(wordcloud)
+library(RmecabKo)
+
+install_mecab("C:/Rlibs/mecab")
+library(RmecabKo)
