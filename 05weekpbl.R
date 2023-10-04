99b504b7efb7f557459093fa151d5a1ca1f2239f
심지원 <simjiwon@simjiwon-ui-MacBookPro.local>
2023-10-04 17:09:06 +0900

    iii

--- 05weekpbl.R
+++ 05weekpbl.R
@@ -8,5 +8,15 @@ library(XML)
 library(wordcloud)
 library(RmecabKo)
 
-install_mecab("C:/Rlibs/mecab")
-library(RmecabKo)
+searchUrl <- 'https://openapi.naver.com/v1/search/news.xml'
+Client_ID <- 'nqwA9gM8SQdvOBr4p5si'
+Client_Secret <- 'LSZ6_28atW'
+
+query <-URLencode(iconv("경영통계","euc-kr", "UTF-8"))
+url <- paste(searchUrl,"?query=", query, "&display=20",sep="")
+
+doc <- getURL(url, httpheader =c('Content-Type' ="apllication/xml", 'X-Naver-CLient-Id' = Client_ID, 'X-Naver-CLient-Secret' =Client_Secret))
+
+doc
+
+xmlFile <- xmlParse(doc)
