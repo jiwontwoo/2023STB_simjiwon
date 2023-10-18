aaead81425f7f21c876155ff6fa37322dc12278a
심지원 <simjiwon@simjiwon-ui-MacBookPro.local>
2023-10-18 16:20:42 +0900

    kk

--- 07. seoul_subway_202212.R
+++ 07. seoul_subway_202212.R
@@ -28,3 +28,25 @@ str(subway)
 subway%>%summarise(on_m=mean(on_board), off_m=mean(getting_off))
 
 max(subway$on_board)
+subway%>%filter(on_board=='승차승객의최대값을 숫자로 넣어주세요')%>%select(Date, Line, Station, on_board)
+
+passenger10 <- subway %>%group_by(Station)%>%summarise(m=mean(total_passenger))%>%arrange(desc(m))%>%
+head(10)
+head(passenger10, 3)
+
+ggplot(data=passenger10,aes(x=reorder(Station, m),y=m))+geom_col()+coord_flip()
+
+subway %>%group_by(Date) %>%summarise(total=sum(total_passenger)) %>%arrange(desc(total)) %>%head(3)
+
+subway %>%filter(Line=="1호선") %>%filter(total_passenger==max(total_passenger)) %>%select(Date, Station, on_board,getting_off, total_passenger)
+
+line_pct <- subway%>%group_by(Line)%>%summarise(total=sum(total_passenger))%>%mutate(all=sum(total),pct=round(total/all*100,2))
+line_pct%>%arrange(desc(pct))%>%head(3)
+
+line_pct10 <- line_pct %>%filter(Line%in%c("1호선","2호선","3호선","4호선","5호선","6호선","7호선","8호선","9호선","분당선" ))
+ggplot(data = line_pct10, aes(x=reorder(Line,pct),y=pct))+geom_col()+coord_flip()+ggtitle("수도권 지하철 노선별 이용비율")+xlab("노선")+ylab("이용비율")
+
+line_graph <- subway %>%
+group_by(day) %>%
+summarise(s=sum(total_passenger))
+ggplot(data = line_graph, aes(x=day, y=s, group=1))+geom_line()+ggtitle("수도권 지하철 일별 이용승객수")+xlab("일")+ylab("이용승객")
